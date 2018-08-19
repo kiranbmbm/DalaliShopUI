9d4e421f066a84b9fbbbc9358362c80fa48be47e
Basavaraj Mudhol <k.basavaraj.mudhol@accenture.com>
2018-08-19 20:22:58 +0530

    added the New DalaliShopUI for the First Time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// routing 
+import {RouterModule,Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OwnersComponent } from './owners/owners.component';
+import { VendorsComponent } from './vendors/vendors.component';
+import { ClientsComponent } from './clients/clients.component';
+import { BuyersComponent } from './buyers/buyers.component';
+import { TransportersComponent } from './transporters/transporters.component';
+import { HelpersComponent } from './helpers/helpers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OwnersComponent,
+    VendorsComponent,
+    ClientsComponent,
+    BuyersComponent,
+    TransportersComponent,
+    HelpersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'Home', component:HomeComponent},
+      {path:'Owners', component:OwnersComponent},
+      {path:'Vendors', component:VendorsComponent},
+      {path:'Clients', component:ClientsComponent},
+      {path:'Buyers', component:BuyersComponent},
+      {path:'Transporters', component:TransportersComponent},
+      {path:'Helpers', component:HelpersComponent},
+      {path:'',redirectTo:'/Home',pathMatch:'full'},
+      {path:'**',component:HomeComponent}   
+     
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
